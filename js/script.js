5dfb827746d15a89aa72babc38bcbf1ff2da47b7
Jessica-vdz <Jessicavdz02@gmail.com>
2025-03-27 12:26:25 +0100

    changed some file paths and made it so the images change when u use the slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var modal = document.getElementById("contactForm");
 var contactLink = document.getElementById("contactLink");
 var closeBtn = document.getElementById("closeBtn");
-
+var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
 // When the user clicks on the "Contact" link, show the modal
 contactLink.onclick = function() {
     modal.style.display = "block";
@@ -19,10 +21,29 @@ window.onclick = function(event) {
     }
 }
 
-let data = {
-    "text": "winter",
-    "img": "/img/winter.png",
+const images = {
+    "fall": "../img/fall.png",
+    "winter": "../img/winter.png",
+    "spring": "../img/spring.png",
+    "summer": "../img/summer.png"
+}
+const text = {
+    "fall":   "Fall",
+    "winter": "Winter",
+    "spring": "Spring",
+    "summer": "Summer"
 }
 
-console.log(data.text);
-console.log(data.img);
+slider.oninput = function(){
+    output.innerHTML = this.value;
+
+    if(this.value == 1){
+        document.getElementById("js--img").src= images.winter;
+    }if(this.value == 2){
+        document.getElementById("js--img").src= images.spring;
+    }if(this.value == 3){
+        document.getElementById("js--img").src= images.summer;
+    }if(this.value == 4){
+        document.getElementById("js--img").src= images.fall;
+    }
+}
